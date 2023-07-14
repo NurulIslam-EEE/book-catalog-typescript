36e10414e06c59f20dd40b3fbd1ccf4b65ae0fc1
NurulIslam-EEE <nurul.islam02310@gmail.com>
2023-07-14 21:32:34 +0600

    firebase config file added

--- src/pages/books/AddBook.tsx
+++ src/pages/books/AddBook.tsx
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddBook() {
+  const [book, setBook] = useState({
+    title: "",
+    image: "",
+    author: "",
+    genre: "",
+  });
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const changedData = { ...book, [e.target.name]: e.target.value };
+    setBook(changedData);
+    // console.log(book);
+  };
+
+  const handleSubmit = () => {};
   return (
     <div className="container">
       <h2>Add Book</h2>
       <form action="">
+        <label htmlFor="">Image</label> <br />
+        <input
+          required
+          onChange={handleOnChange}
+          type="text"
+          name="image"
+        />{" "}
+        <br />
         <label htmlFor="">Title</label> <br />
-        <input type="Text" name="Title" /> <br />
+        <input
+          required
+          onChange={handleOnChange}
+          type="Text"
+          name="title"
+        />{" "}
+        <br />
         <label htmlFor="">Author</label> <br />
-        <input type="text" name="Author" /> <br />
+        <input
+          required
+          onChange={handleOnChange}
+          type="text"
+          name="author"
+        />{" "}
+        <br />
         <label htmlFor="">Genre</label> <br />
-        <input type="text" name="Genre" /> <br />
+        <input
+          required
+          onChange={handleOnChange}
+          type="text"
+          name="genre"
+        />{" "}
+        <br />
+        <button>Submit</button>
       </form>
     </div>
   );
