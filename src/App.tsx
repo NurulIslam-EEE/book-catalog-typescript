d11427ad1a1673ba2d032bac0bc8746ed66cb2a8
NurulIslam-EEE <nurul.islam02310@gmail.com>
2023-07-14 21:47:15 +0600

    navigation added

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home/Home";
 import AddBook from "./pages/books/AddBook";
 import NotFound from "./pages/notFound/NotFound";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./components/common/Navigation";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Navigation />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/add-book" element={<AddBook />} />
---
+++ src/components/common/Navigation.tsx
@@ -0,0 +1,33 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { useAppSelector } from "../../redux/hooks";
+
+function Navigation() {
+  const { user } = useAppSelector((state) => state.user);
+
+  console.log("uuuuuu", user);
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="ms-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
