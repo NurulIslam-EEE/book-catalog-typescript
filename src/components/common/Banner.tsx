0230d6631f1a2d5edb5ae0fc3e411e04b484105e
NurulIslam-EEE <nurul.islam02310@gmail.com>
2023-07-16 02:15:09 +0600

    home page banner added

---
+++ src/components/common/Banner.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div className="banner-container">
+      <img
+        src="https://wafilife-media.wafilife.com/uploads/2023/03/lekhokder-hand_written_bookmark_offer_Desktop-min.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Banner;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Banner from "../../components/common/Banner";
+import "./home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <Banner />
+      <h3>Recent Books</h3>
+    </div>
+  );
 }
 
 export default Home;
